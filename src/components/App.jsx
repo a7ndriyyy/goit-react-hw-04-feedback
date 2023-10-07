a374558cc0cb53b227cd0f5507bc5ff3e18db27a
a7ndriyyy <andrii.nastych@gmail.com>
2023-10-07 12:43:28 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,8 +8,10 @@ export const App = () => {
 
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
-   const [bad, setBad] = useState(0);
-  const feedbackNames = [ good, neutral, bad ];
+  const [bad, setBad] = useState(0);
+  
+  const feedbackNames = [good, neutral, bad];
+  
 
  const countTotalFeedback = () => {
    return good + neutral + bad;
